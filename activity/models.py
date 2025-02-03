0d8ceeb76512a1521c56d757e6977fcb877b4362
eleshi18 <eleshi18@highgateschool.org.uk>
2025-02-03 16:02:12 +0000

    committed

--- activity/models.py
+++ activity/models.py
@@ -9,14 +9,23 @@ class Base(so.DeclarativeBase):
 person_activities = sa.Table('person_activities',
                              Base.metadata,
                              sa.Column('id', sa.Integer, primary_key=True),
-                             sa.Column('activity_id', sa.ForeignKey('activity.id')),
-                             sa.Column('person_id', sa.ForeignKey('person.id')),
+                             sa.Column('activity_id', sa.ForeignKey('activities.id')),
+                             sa.Column('person_id', sa.ForeignKey('persons.id')),
                              sa.UniqueConstraint('activity_id', 'person_id')
                              )
 
 class Activity(Base):
     __tablename__ = 'activities'
-    id = so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
+    id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
+    name: so.Mapped[str] = so.mapped_column(unique=True)
+    attendees: so.Mapped[list["Person"]] = so.relationship("Person",
+                                                           secondary=person_activities,
+                                                           order_by='(Person.last_name, Person.first_name)',
+                                                           back_populates="activities")
+
+    def __repr__(self) -> str:
+        return f"Activity(name='{self.name}')"
+
 
 
 class Person(Base):
@@ -24,6 +33,11 @@ class Person(Base):
     id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
     first_name: so.Mapped[Optional[str]]
     last_name: so.Mapped[str]
+    activities: so.Mapped[list[Activity]] = so.relationship("Activity",
+                                                            secondary=person_activities,
+                                                            order_by='Activity.name',
+                                                            back_populates="attendees")
+
 
 def __repr__(self) -> str:
     return f"Person(first_name='{self.first_name}', last_name='{self.last_name}')>"
--- activity/write_to_db.py
+++ activity/write_to_db.py
@@ -1,16 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from models import Person
+from models import Person, Activity
 
-andrew = Person(first_name="Andrew", last_name="Dales")
 people = [Person(first_name="Eleanore", last_name="Shiner"),
           Person(first_name='Snake', last_name="Charmer"),
           Person(first_name='Dennis', last_name="Zazuliak"),
           ]
 
+chess = Activity(name="Chess")
+fives = Activity(name="Fives")
+outdoor_ed = Activity(name="Outdoor Ed")
+
+people[0].activities.append(chess)
+people[0].activities.append(fives)
+people[1].activities.append(outdoor_ed)
+people[1].activities.append(fives)
+
 engine = create_engine('sqlite:///activities.sqlite', echo=True)
 
 with Session(engine) as sess:
-    sess.add(andrew)
     sess.add_all(people)
     sess.commit()
+
