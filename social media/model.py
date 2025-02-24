2e8c25f2c87ef8aeb254553c99d96567362926db
eleshi18 <eleshi18@highgateschool.org.uk>
2025-02-24 15:09:34 +0000

    committed

--- social media/model.py
+++ social media/model.py
@@ -6,28 +6,24 @@ import sqlalchemy.orm as so
 class Base(so.DeclarativeBase):
     pass
 
-class Like(Base):
-    __tablename__ = "likes"
-    id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
-    likers: so.Mapped[list["User"]] = so.relationship("User", order_by="(User.name)", back_populates="likes")
-    post_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("posts.id"))
-    post: so.Mapped[Optional["Post"]] = so.relationship("Post", back_populates="likes")
 
-    def __repr__(self) -> str:
-        return f"Like(id={self.id}, post_id={self.post_id})"
+likes_table = sa.Table(
+    'likes',
+    Base.metadata,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('user_id', sa.Integer, sa.ForeignKey('users.id'), nullable=False),
+    sa.Column('post_id', sa.Integer, sa.ForeignKey('posts.id'), nullable=False),
+)
 
 class User(Base):
     __tablename__ = 'users'
     id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
-    name: so.Mapped[Optional[str]]
-    age: so.Mapped[int]
-    gender: so.Mapped[Optional[str]]
-    nationality: so.Mapped[Optional[str]]
-    likes: so.Mapped[list[Like]] = so.relationship("Like", order_by="Like.post_id", back_populates="likers")
-    posts: so.Mapped[list["Post"]] = so.relationship("Post", order_by="Post.id", back_populates="user")
-
-    def greeting(self) -> None:
-        print(f"Hello, my name is {self.name}")
+    name: so.Mapped[Optional[str]] = so.mapped_column(unique=True)
+    age: so.Mapped[int|None]
+    gender: so.Mapped[Optional[str|None]]
+    nationality: so.Mapped[Optional[str|None]]
+    liked_posts: so.Mapped[list['Post']] = so.relationship
+    posts: so.Mapped[list["Post"]] = so.relationship( back_populates="user")
 
     def __repr__(self) -> str:
         return f"User(id={self.id}, name={self.name}, age={self.age}, gender={self.gender})"
@@ -38,8 +34,22 @@ class Post(Base):
     title: so.Mapped[str]
     description: so.Mapped[str]
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("users.id"))
-    user: so.Mapped[User] = so.relationship("User", back_populates="posts")
-    likes: so.Mapped[list[Like]] = so.relationship("Like", back_populates="post")
+    user: so.Mapped["User"] = so.relationship("User", back_populates="posts")
+    likes: so.Mapped[list["User"]] = so.relationship(secondary=likes_table,
+                                                     back_populates='liked_posts')
+    comments: so.Mapped[list["Comment"]] = so.relationship(back_populates='post')
 
     def __repr__(self) -> str:
         return f"Post(id={self.id}, title={self.title}, description={self.description})"
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id: so.Mapped[int] = so.mapped_column(primary_key=True, autoincrement=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('users.id'))
+    post_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('posts.id'))
+    comment: so.Mapped[str]
+    post: so.Mapped['Post'] = so.relationship(back_populates='comments')
+    user: so.Mapped['User'] = so.relationship(back_populates='comments_made')
+
+    def __repr__(self):
+        return f"Comment(user_id={self.user_id}, post_id={self.post_id}, comment='{self.comment}')"
--- social media/write_to_db.py
+++ social media/write_to_db.py
@@ -1,36 +1,51 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model import User, Like, Post
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from model import User, Post, Comment, Base
+
+engine = sa.create_engine('sqlite:///social_media.db', echo=True)
+session = so.Session(bind=engine)
 
 # Create people
-Users = [
+users = [
     User(name="shiney", age=100000, gender="girly", nationality="american"),
     User(name="snakecharmer", age=9, gender="buddy", nationality="british"),
     User(name="Dennis", age=17, gender="tree", nationality="forest"),
 ]
 
 # Check and create or reuse locations
-def get_or_create_like(session, post_title):
-    post = session.query(Post).filter_by(room=post_title).first()
-    if not post:
-        post = Post(title = post_title)
-        session.add(post)
-        session.add(post)
-    return post
+posts = [
+    Post(title=":)", description="Super cool stuff"),
+    Post(title=":(", description="Super not cool stuff"),
+    Post(title="<3", description="yeeehawww"),
+]
 
-post1 = Post(title="post 1")
+for i in range(len(users)):
+    users[i].posts.append(posts[i])
 
-# Assign activities to people
-people[0].activities.append(chess)
-people[0].activities.append(fives)
-people[1].activities.append(outdoor_ed)
-people[1].activities.append(drawing)
+# Add likes
+users[0].liked_posts.append(posts[1])
+users[0].liked_posts.append(posts[2])
+users[1].liked_posts.append(posts[0])
+users[1].liked_posts.append(posts[3])
+users[2].liked_posts.append(posts[0])
+users[2].liked_posts.append(posts[3])
 
-# Insert into the database
-engine = create_engine('sqlite:///activities.sqlite', echo=True)
+session.add_all(users)
+session.commit()
 
-with Session(engine) as sess:
-    location_chess = get_or_create_location(sess, '7')
-    location_fives = get_or_create_location(sess, 'Fives court')
-    sess.add_all(people)
-    sess.commit()
+comments = [
+    Comment(user_id=users[1].id, comment="agreed"),
+    Comment(user_id=users[2].id, comment="agreed"),
+    Comment(user_id=users[3].id, comment="agreed"),
+    Comment(user_id=users[3].id, comment="disagree"),
+    Comment(user_id=users[0].id, comment="disagree"),
+]
+
+posts[0].comments.append(comments[0])
+posts[0].comments.append(comments[1])
+posts[1].comments.append(comments[2])
+posts[2].comments.append(comments[3])
+posts[3].comments.append(comments[4])
+
+session.add_all(comments)
+session.commit()
