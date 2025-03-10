1ae054082537fd0dd32afbae893b23a60908fe58
eleshi18 <eleshi18@highgateschool.org.uk>
2025-03-10 15:08:32 +0000

    committed

---
+++ social media/test_db.py
@@ -0,0 +1,34 @@
+import pytest
+import sqlalchemy.orm as so
+import sqlalchemy as sa
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
+
+from models import User, Post, Comment, Base
+
+test_db_location = 'sqlite:///:memory:'
+
+def test_test():
+    assert 3**2 == 9
+
+class TestDatabase:
+    @pytest.fixture(scope='class')
+    def db_session(self):
+        engine = sa.create_engine(test_db_location)
+        Base.metadata.create_all(engine)
+        session = so.Session(engine)
+        yield session
+        session.close()
+        Base.metadata.drop_all(engine)
+
+    def test_valid_user(self, db_session):
+        user = User(name="Rayhan", age = 20, gender="male")
+        db_session.add(user)
+        db_session.commit()
+        qry = sa.select(User).where(User.name == "Rayhan")
+        rayhan = db_session.scalar(qry)
+        assert rayhan is not None
+        assert rayhan.name == "Rayhan"
+        assert rayhan.age == 20
+        assert rayhan.gender == "male"
+        assert rayhan.nationality is None
